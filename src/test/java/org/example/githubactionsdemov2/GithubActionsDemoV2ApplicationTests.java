a51f88fd3aa39a8820af8e13b23a3a3e9f239589
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2025-02-24 14:12:48 +0100

    Initial

---
+++ src/test/java/org/example/githubactionsdemov2/GithubActionsDemoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubactionsdemov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
